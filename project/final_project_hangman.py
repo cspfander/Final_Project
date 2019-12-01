c3e79fcb65b2354543edec900a223484d4812a15
packe <cspfander@dmacc.edu>
2019-12-01 16:27:13 -0600

    Added a try/except, start time, and showing the user how long the magic word is

--- project/final_project_hangman.py
+++ project/final_project_hangman.py
@@ -9,6 +9,7 @@ The purpose of this program is to make a GUI that will allow the user to play th
 import random
 import tkinter as tk
 from tkinter import messagebox
+from datetime import datetime
 
 
 class LetterGuesser:
@@ -33,6 +34,7 @@ class LetterGuesser:
 
 def guess():
     allowed_guess_input = set("abcdefghijklmnopqrstuvwxyz")
+    letter_guessed = new_guess.get().lower()
     if allowed_guess_input.issuperset(new_guess.get().lower()):
         if len(letter_guessed) < 0 or len(letter_guessed) > 1:
             raise InvalidGuess("Please only guess 1 letter!")
@@ -52,6 +54,8 @@ def start_game():
     :return:
     """
     new_game = LetterGuesser()
+    length_of_magic_word = len(new_game.magic_word)
+    tk.Label(m, text="The word is " + str(length_of_magic_word) + " letters long!").grid(row=2)
     print(new_game.magic_word)
 
 
@@ -61,20 +65,25 @@ class InvalidGuess(Exception):
 
 
 if __name__ == '__main__':
-    global letters_for_game
-    letters_for_game = LetterGuesser()
-    m = tk.Tk()
-    m.title("Hangman")
-    start_game_button = tk.Button(m, text="Start Game!", command=start_game, width=33)
-    start_game_button.grid(row=1, columnspan=2)
-    tk.Label(m, text="Please guess a letter:").grid(row=2, columnspan=2)
-    new_guess = tk.Entry(m)
-    new_guess.grid(row=3)
-    letter_guessed = new_guess.get().lower()
-    guess_button = tk.Button(m, text="Guess", command=guess, width=14)
-    guess_button.grid(row=3, column=1)
-    tk.Label(m, text="Already guessed letters:").grid(row=4)
-    tk.Label(m, text=letters_for_game.guessed_list).grid(row=4, column=1)
-    exit_button = tk.Button(m, text="Exit", command=m.destroy, width=16)
-    exit_button.grid(row=5, columnspan=2)
-    m.mainloop()
+    try:
+        global letters_for_game
+        letters_for_game = LetterGuesser()
+        m = tk.Tk()
+        m.title("Hangman")
+        start_game_button = tk.Button(m, text="Start Game!", command=start_game, width=33)
+        start_game_button.grid(row=1, columnspan=2)
+        tk.Label(m, text="Please guess a letter:").grid(row=3, columnspan=2)
+        new_guess = tk.Entry(m)
+        new_guess.grid(row=4)
+        guess_button = tk.Button(m, text="Guess", command=guess, width=14)
+        guess_button.grid(row=4, column=1)
+        tk.Label(m, text="Already guessed letters:").grid(row=5)
+        tk.Label(m, text=letters_for_game.guessed_list).grid(row=5, column=1)
+        exit_button = tk.Button(m, text="Exit", command=m.destroy, width=16)
+        exit_button.grid(row=6, columnspan=2)
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        tk.Label(m, text="Start Time: " + current_time).grid(row=7, column=1)
+        m.mainloop()
+    except ValueError:
+        print("The program has been closed due to a ValueError!")
